ba60357c464afb905370546cfa44585b46dc526a
Abid Hossain <hosainabid@gmail.com>
2021-06-29 15:25:56 +0600

    Update Price

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let subtotalPrice = 0;
 fetch('/data.json')
     .then(res => res.json())
     .then(data => displayProduct(data))
@@ -29,6 +30,15 @@ const handleClick = (id, product) => {
     const cardBody = document.getElementById('cart-body');
     const productCard = document.createElement('div');
     productCard.className = `shopping-cart ${id}`;
+
+    // Add Sub Total Price
+    subtotalPrice += product.product_price;
+
+    let subTotalElement = document.getElementById('subTotal');
+    subTotalElement.innerText = subtotalPrice;
+    let payNowElement = document.getElementById('payNow');
+    payNowElement.innerText = subtotalPrice;
+
     const productCardInfo = `
             <div class="row cart-items">
                 <div class="col-3">
@@ -56,25 +66,45 @@ const handleClick = (id, product) => {
     for (let i = 0; i < removeItems.length; i++) {
         const button = removeItems[i];
         button.addEventListener('click', function (event) {
+            if (product.product_price === parseFloat(event.target.parentElement.parentElement.childNodes[7].childNodes[1].childNodes[1].childNodes[0].data)) {
+                updateTotal(parseFloat(event.target.parentElement.parentElement.childNodes[7].childNodes[1].childNodes[1].childNodes[0].data));
+                console.log(parseFloat(event.target.parentElement.parentElement.childNodes[7].childNodes[1].childNodes[1].childNodes[0].data));
+            }
+            
             let deleteBtn = event.target;
-            deleteBtn.parentElement.parentElement.remove();
-            updateTotal();
+            deleteBtn.parentElement.parentElement.parentElement.remove();
         })
     }
 }
 
-const updateTotal = () => {
-    let cartItemContainer = document.getElementsByClassName('shopping-cart')[0];
-    let cartRows = cartItemContainer.getElementsByClassName('cart-items');
-    total = 0;
-    for (let i = 0; i < cartRows.length; i++) {
-        let cartRow = cartRows[i];
-        const cartPrice = cartRow.getElementsByClassName('product-price')[i];
-        const quantity = cartRow.getElementsByClassName('quantity')[i];
-        console.log(quantity, cartPrice);
-        let price = parseFloat(cartPrice);
-        let quantityElement = quantity.value;
-        total = total + (price * quantity);
-    }
-    document.getElementsByClassName('total-price')[0].innerText = total;
+const updateTotal = (price) => {
+    // let cartItemContainer = document.getElementsByClassName('shopping-cart')[0];
+    // Add Sub Total Price
+    subtotalPrice -= price;
+
+    let subTotalElement = document.getElementById('subTotal');
+    subTotalElement.innerText = subtotalPrice;
+    let payNowElement = document.getElementById('payNow');
+    payNowElement.innerText = subtotalPrice;
+    console.log(subtotalPrice);
+    // console.log(price);
+
+
+
+
+
+
+
+    // let cartRows = cartItemContainer.getElementsByClassName('cart-items');
+    // total = 0;
+    // for (let i = 0; i < cartRows.length; i++) {
+    //     let cartRow = cartRows[i];
+    //     const cartPrice = cartRow.getElementsByClassName('product-price')[i];
+    //     const quantity = cartRow.getElementsByClassName('quantity')[i];
+    //     console.log(quantity, cartPrice);
+    //     let price = parseFloat(cartPrice);
+    //     let quantityElement = quantity.value;
+    //     total = total + (price * quantity);
+    // }
+    // document.getElementsByClassName('total-price')[0].innerText = total;
 }
